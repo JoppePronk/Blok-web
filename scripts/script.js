8a9c90d9143704c7a03a1d16cc157ba02e93ea87
JoppePronk <74979466+JoppePronk@users.noreply.github.com>
2021-09-23 15:09:37 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+
+
+var deButton = document.querySelector("nav button");
+var deNav = document.querySelector("nav");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav.classList.toggle("toonMenu");
+}
+
+console.log(hallo);
